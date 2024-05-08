9b62ad1b1d180012eb51ce270e611d3141f30a39
uhcardoso <werlendev@gmail.com>
2024-05-08 19:01:15 -0300

    estrutura de login e registro de usuarios

--- auth.php
+++ auth.php
@@ -2,7 +2,53 @@
 include_once("templates/header.php");
 ?>
 <div id="main-container" class="container-fuid">
-    <h1>Autenticação</h1>
+    <div class="col-md-12">
+        <div class="row" id="auth-row">
+            <div class="col-md-4" id="login-container">
+                <h2>Entrar</h2>
+                <form action="#" method="POST">
+                    <input type="hidden" name="type" value="login" />
+                    <div class="form-group">
+                        <label for="email">E-mail:</label>
+                        <input type="email" class="form-control" placeholder="Digite seu e-mail" id="email" name="email" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Senha</label>
+                        <input type="password" class="form-control" placeholder="Digite sua senha" id="password" name="password" required />
+                    </div>
+                    <input type="submit" class="btn card-btn" value="Entrar" />
+                </form>
+            </div>
+
+            <div class="col-md-4" id="register-container">
+                <h2>Criar conta</h2>
+                <form action="#" method="POST">
+                    <input type="hidden" name="type" value="register" />
+                    <div class="form-group">
+                        <label for="email">E-mail:</label>
+                        <input type="email" class="form-control" placeholder="Digite seu e-mail" id="email" name="email" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Nome:</label>
+                        <input type="text" class="form-control" placeholder="Digite seu nome" id="name" name="name" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="lastname">Sobrenome:</label>
+                        <input type="text" class="form-control" placeholder="Digite seu sobrenome" id="lastname" name="lastname" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Senha:</label>
+                        <input type="password" class="form-control" placeholder="Digite sua senha" id="password" name="password" required />
+                    </div>
+                    <div class="form-group">
+                        <label for="confirmPassword">Confirmação de senha:</label>
+                        <input type="password" class="form-control" placeholder="Confirme sua senha" id="confirmPassword" name="confirmPassword" required />
+                    </div>
+                    <input type="submit" class="btn card-btn" value="Registrar" />
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 <?php
 include_once("templates/footer.php");
