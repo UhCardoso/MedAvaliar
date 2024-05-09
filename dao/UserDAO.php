8b184566b32593030c7306681fe3f3339a7cf3eb
uhcardoso <werlendev@gmail.com>
2024-05-09 20:29:35 -0300

    finalizando registro e logout de usuario

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -59,6 +59,26 @@ class UserDAO implements UserDAOInterface
 
     public function verifyToken($protected = false)
     {
+        if (!empty($_SESSION["token"])) {
+            // pega o token da ssesion
+            $token = $_SESSION["token"];
+
+            $user = $this->findByToken($token);
+
+            if ($user) {
+                return $user;
+            }
+
+            if ($protected) {
+                // redireciona usuario nao identificado
+                $this->message->setMessage("Faça autenticação para acessar esta página!", "error", "/index.php");
+            }
+        }
+
+        if ($protected) {
+            // redireciona usuario nao identificado
+            $this->message->setMessage("Faça autenticação para acessar esta página!", "error", "/index.php");
+        }
     }
 
     public function setTokenToSession($token, $redirect = true)
@@ -67,7 +87,7 @@ class UserDAO implements UserDAOInterface
         $_SESSION["token"] = $token;
 
         if ($redirect) {
-            // redireciona para p perfil do usuario
+            // redireciona para o perfil do usuario
             $this->message->setMessage("Seja bem vindo!", "success", "/editprofile.php");
         }
     }
@@ -103,6 +123,32 @@ class UserDAO implements UserDAOInterface
 
     public function findByToken($token)
     {
+        if ($token != "") {
+            $stmt = $this->conn->prepare("SELECT * FROM users WHERE token = :token");
+            $stmt->bindParam(":token", $token);
+
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+                $data = $stmt->fetch();
+                $user = $this->buildUser($data);
+
+                return $user;
+            } else {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    public function destroyToken()
+    {
+        // remove token da session
+        $_SESSION["token"] = "";
+
+        // redirecionar e apresentar a mensagem de sucesso
+        $this->message->setMessage("Você fez o logout com sucesso!", "success", "/index.php");
     }
 
     public function changePassword(User $user)
--- editprofile.php
+++ editprofile.php
@@ -1,5 +1,11 @@
 <?php
 include_once("templates/header.php");
+require_once("dao/UserDAO.php");
+
+$userDao = new UserDAO($conn, $BASE_URL);
+
+$userData = $userDao->verifyToken(true);
+
 ?>
 <div id="main-container" class="container-fuid">
     <h1>Edição de perfil</h1>
