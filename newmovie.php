41984e4e2d4124c1d79085a7b00364cf2844ecf9
uhcardoso <werlendev@gmail.com>
2024-07-02 19:40:53 -0300

    adicionando filmes

--- newmovie.php
+++ newmovie.php
@@ -15,19 +15,19 @@ $userData = $userDao->verifyToken(true);
     <div class="offset-md-4 col-md-4 new-movie-container">
         <h1 class="page-title">Adicionar filme</h1>
         <p class="page-description">Adicione sua crítica e compartilhe com o mundo!</p>
-        <form action="<?= $BASE_URL ?>/movie_process.php" id="add-movie-form" method="POST" enctype="multpart/form-date">
+        <form action="<?= $BASE_URL ?>/movie_process.php" id="add-movie-form" method="POST" enctype="multipart/form-data">
             <input type="hidden" name="type" value="create" />
             <div class="form-group">
                 <label for="title">Título:</label>
-                <input class="form-control" type="text" id="title" placeholder="Digite um título para o seu filme" />
+                <input name="title" class="form-control" type="text" id="title" placeholder="Digite um título para o seu filme" />
             </div>
             <div class="form-group">
                 <label for="image">Imagem:</label><br>
-                <input type="file" class="form-control-file" name="image" id="image" />
+                <input type="file" class="form-control-file" name="image" id="image" value="teste" />
             </div>
             <div class="form-group">
                 <label for="length">Duração:</label>
-                <input type="text" class="form-control" name="length" id="length" placeholder="Digite a duração do filme" />
+                <input type="text" name="length" class="form-control" name="length" id="length" placeholder="Digite a duração do filme" />
             </div>
             <div class="form-group">
                 <label for="category">Categoria:</label>
