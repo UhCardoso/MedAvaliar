c86b20df8654f1135cdd661a9b12c44a083acdc6
uhcardoso <werlendev@gmail.com>
2024-07-03 21:09:17 -0300

    exibindo filmes na dashboard

--- index.php
+++ index.php
@@ -1,8 +1,46 @@
 <?php
 include_once("templates/header.php");
+require_once("dao/MovieDAO.php");
+
+//dao dos filmes 
+$movieDao = new MovieDAO($conn, $BASE_URL);
+
+$latestMovies = $movieDao->getLatestMovies();
+
+$actionMovies = $movieDao->getMoviesByCategory("Ação");
+$comedyMovies = $movieDao->getMoviesByCategory("Comédia");
 ?>
 <div id="main-container" class="container-fluid">
-    <h1>corp site</h1>
+    <h2 class="section-title">Filmes novos</h2>
+    <p class="section-description">Veja as críticas dos últimaos filmes adicionados no MovieStar</p>
+    <div class="movies-container">
+        <?php foreach ($latestMovies as $movie) : ?>
+            <?php require("templates/movie_card.php"); ?>
+        <?php endforeach; ?>
+        <?php if (count($latestMovies) === 0) : ?>
+            <p class="empty-list">ainda não há filmes cadastrados</p>
+        <?php endif; ?>
+    </div>
+    <h2 class="section-title">Ação</h2>
+    <p class="section-description">Veja os melhores filmes de ação</p>
+    <div class="movies-container">
+        <?php foreach ($actionMovies as $movie) : ?>
+            <?php require("templates/movie_card.php"); ?>
+        <?php endforeach; ?>
+        <?php if (count($actionMovies) === 0) : ?>
+            <p class="empty-list">ainda não há filmes de ação cadastrados</p>
+        <?php endif; ?>
+    </div>
+    <h2 class="section-title">Comédia</h2>
+    <p class="section-description">Veja os melhores filmes de comédia</p>
+    <div class="movies-container">
+        <?php foreach ($comedyMovies as $movie) : ?>
+            <?php require("templates/movie_card.php"); ?>
+        <?php endforeach; ?>
+        <?php if (count($comedyMovies) === 0) : ?>
+            <p class="empty-list">ainda não há filmes de comédia cadastrados</p>
+        <?php endif; ?>
+    </div>
 </div>
 <?php
 include_once("templates/footer.php");
